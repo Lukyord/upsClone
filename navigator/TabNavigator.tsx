d191f6a19bbb64b9fc474a8590ff98ca1100e732
Lukyord <ttansiri524@gmail.com>
2023-02-10 11:08:54 +0100

    install bottomTabsNavigator and add OrdersScreen

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CustomersScreen from "../screens/CustomersScreen/CustomersScreen";
+import OrdersScreen from "../screens/OrdersScreen/OrdersScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <View>
-      <Text>TabNavigator</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Customers" component={CustomersScreen} />
+      <Tab.Screen name="Orders" component={OrdersScreen} />
+    </Tab.Navigator>
   );
 }
